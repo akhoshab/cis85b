402827a91d705e829670895191362b7c8d478d27
akhoshab <35505066+akhoshab@users.noreply.github.com>
2018-07-29 13:29:01 -0700

    Add files via upload

--- lab0/DrawPanel.java
+++ lab0/DrawPanel.java
@@ -1,9 +1,13 @@
-# cis85b-lab0
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package javaapplication10;
 
 /**
  *
- * @author ayla
+ * @author ashur
  */
 import java.awt.Color;
 import java.awt.EventQueue;
